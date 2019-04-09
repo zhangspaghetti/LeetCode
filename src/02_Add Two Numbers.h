d866c625b98940ba6b9229f192071e1941630bd6
Zhang Shaocong <zhangspaghetti@gmail.com>
2019-04-09 20:20:59 +0800

    optimize 02_Add Two Numbers

--- src/02_Add Two Numbers.h
+++ src/02_Add Two Numbers.h
@@ -1,86 +1,40 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode(int x) : val(x), next(NULL) {}
+* };
+*/
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         int carry = 0;
-        int unit = 0;
         int sum = 0;
-        ListNode *head, *l, *previous;
+        ListNode *head, *l;
         
+        //Dummy head
         head = new ListNode(0);
         l = head;
         
-        while(l1 != NULL && l2 != NULL)
+        while(l1 != NULL || l2 != NULL)
         {
-            sum = l1->val + l2->val + carry;
-            unit = sum % 10;
-            l->val = unit;
+            int x = l1 != NULL ? l1->val : 0;
+            int y = l2 != NULL ? l2->val : 0;
+            sum = x + y + carry;
             carry = sum / 10;
             
-            l->next = new ListNode(0);
-            previous = l;
+            l->next = new ListNode(sum % 10);
             l = l->next;
-            l1 = l1->next;
-            l2 = l2->next;
-        }
-        if(l1 == NULL)
-        {
-            while(l2 != NULL)
-            {
-                sum = l2->val + carry;
-                unit = sum % 10;
-                l->val = unit;
-                carry = sum / 10;
-            
-                l->next = new ListNode(0);
-                previous = l;
-                l = l->next;
-                l2 = l2->next;
-            }
-            if(carry > 0)
-            {
-                l->val = carry;
-                l->next = NULL;
-            }
-            else
-            {
-                delete l;
-                previous->next = NULL;
-            }
-        }
-        else if(l2 == NULL)
-        {
-            while(l1 != NULL)
-            {
-                sum = l1->val + carry;
-                unit = sum % 10;
-                l->val = unit;
-                carry = sum / 10;
-            
-                l->next = new ListNode(0);
-                previous = l;
-                l = l->next;
-                l1 = l1->next;
-            }
-            if(carry > 0)
-            {
-                l->val = carry;
-                l->next = NULL;
-            }
-            else
-            {
-                delete l;
-                previous->next = NULL;
-            }
+            if(l1 != NULL) l1 = l1->next;
+            if(l2 != NULL) l2 = l2->next;
         }
         
-        return head;
+        if(carry > 0)
+        {
+            l->next = new ListNode(carry);
+        }
+        
+        return head->next;
     }
 };
